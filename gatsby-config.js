849641b51d7e09f7ca6c8a804e30f37d36196550
John Ibsen <jibsen@indigoag.com>
2020-03-30 17:10:55 -0400

    Initial pass at generator website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: `/passport-title-generator-website`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Passport Title Generator`,
+    description: `Randomizer for generating new fun titles for your Animal Crossing: New Horizons passport.`,
+    author: `@magichair`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +19,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `passport-title-generator-website`,
+        short_name: `passport-title`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#21ed0e`,
+        theme_color: `#21ed0e`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ac-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
